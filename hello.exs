c07ecfcf348e1aa7b4f9df29c83cd4bb81b50c29
pmeinhardt <mnhrdt@gmail.com>
2016-10-14 00:29:11 +0200

    Pattern matching for the win

--- hello.exs
+++ hello.exs
@@ -80,22 +80,6 @@ defmodule GettingToKnowElixir do
     assert fn -> false end
   end
 
-  test "comparison operators (==, ===, <, ...)" do
-    # === is more strict about types when comparing numbers
-
-    assert 1 == 1.0
-    refute 1 != 1.0
-
-    refute 1 === 1.0
-    assert 1 !== 1.0
-
-    # all types are comparable (excerpt)
-
-    assert 1 < "1"
-
-    assert 999 < :a < (fn -> nil end) < {:a} < [:a]
-  end
-
   test "anonymous functions & closures" do
     add = fn a -> (fn b -> a + b end) end
     inc = add.(1)
@@ -113,7 +97,7 @@ defmodule GettingToKnowElixir do
     puts = &IO.puts/1 # capture
 
     assert capture_io(fn -> puts.(square.(13)) end) == "169\n"
-    
+
     assert (fn a -> a > 0 end).(1) # immediately invoked function expression
   end
 
@@ -126,6 +110,7 @@ defmodule GettingToKnowElixir do
     assert (ls -- [1]) == [0, 1]
     assert (ls ++ [2]) == [0, 1, 1, 2]
     assert ((ls -- [1]) ++ [2]) == [0, 1, 2]
+    assert [0 | ls] == [0, 0, 1, 1]
   end
 
   test "tuples" do
@@ -145,5 +130,109 @@ defmodule GettingToKnowElixir do
     assert tp2 != tp
     assert tp == {:zero, 1, "two"}
     assert tp2 == {:zero, :one, "two"}
+  end
+
+  test "comparison operators (==, ===, <, ...)" do
+    # === is more strict about types when comparing numbers
+
+    assert 1 == 1.0
+    refute 1 != 1.0
+
+    refute 1 === 1.0
+    assert 1 !== 1.0
+
+    # all types are comparable (excerpt)
+
+    assert 1 < "1"
+
+    assert 0 < :nil < (fn -> nil end) < {nil} < [nil]
+  end
+
+  test "more term ordering" do
+    assert :ni < :nil
+    assert :nil < :nim
+
+    assert "a" < "aa"
+    assert "aa" < "ab"
+
+    assert {0} < {nil}
+    assert {0} < {0, 0}
+    assert {0, 0} < {0, 1}
+
+    assert [0] < [nil]
+    assert [0] < [0, 0]
+    assert [0, 0] < [0, 1]
+  end
+
+  test "match operator" do
+    x = 1
+    assert (1 = x) == 1
+    assert_raise MatchError, fn -> 2 = x end
+  end
+
+  test "pattern matching" do
+    {a, b, c} = {0, :one, "two"}
+
+    assert a == 0
+    assert b == :one
+    assert c == "two"
+
+    assert_raise MatchError, fn -> {x, y} = {0} end
+    assert_raise MatchError, fn -> {x} = {0, 1} end
+    assert_raise MatchError, fn -> {x} = [0, 1] end
+
+    {:ok, d} = {:ok, 201}
+
+    assert d == 201
+
+    assert_raise MatchError, fn -> {:ok, x} = {:no, 500} end
+
+    [e, f, g] = [3, :four, "five"]
+
+    assert e == 3
+    assert f == :four
+    assert g == "five"
+
+    assert_raise MatchError, fn -> [x, y] = [0] end
+    assert_raise MatchError, fn -> [x] = [0, 1] end
+
+    [h | t] = [1, 2, 3]
+
+    assert h == 1
+    assert t == [2, 3]
+
+    [h | _] = [1, 2, 3] # ignore tail (_ cannot be read from)
+
+    assert_raise MatchError, fn -> [x | y] = [] end
+
+    {z, z} = {1, 1}
+    assert z == 1
+
+    assert_raise MatchError, fn -> {q, q} = {1, 2} end
+
+    {r, _, _} = {0, 1, 2} # _ can match different values
+    assert r == 0
+  end
+
+  test "nested pattern matching" do
+    [[a, b], [c, d]] = [[1, 2], [3, 4]]
+
+    assert a == 1
+    assert b == 2
+    assert c == 3
+    assert d == 4
+  end
+
+  test "pin operator" do
+    x = 1 # bind (variable on lhs)
+    x = 2 # rebind
+
+    assert_raise MatchError, fn -> 1 = x end # std. match (variable on rhs)
+    assert_raise MatchError, fn -> ^x = 1 end # do not rebind, but match
+
+    {^x, y} = {2, 3} # prevent rebinding of x, instead match {2, y} = {2, 3}
+    assert y == 3
+
+    assert_raise MatchError, fn -> {^x, z} = {0, 3} end
   end
 end
